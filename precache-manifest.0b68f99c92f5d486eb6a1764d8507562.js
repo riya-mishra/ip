d48f937b7aa559b6cb6e7278038986b49ff95618
riya-mishra <bittuasssd22@gmail.com>
2019-10-11 22:18:27 +0530

    Updates

---
+++ precache-manifest.0b68f99c92f5d486eb6a1764d8507562.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9acd7b558b0bf93cb614cbdc23aa18e",
+    "url": "/ip/index.html"
+  },
+  {
+    "revision": "edbec9561b4871401d81",
+    "url": "/ip/static/css/main.94734735.chunk.css"
+  },
+  {
+    "revision": "471520a931dcf448318b",
+    "url": "/ip/static/js/2.d272a10c.chunk.js"
+  },
+  {
+    "revision": "edbec9561b4871401d81",
+    "url": "/ip/static/js/main.1ff7ffa0.chunk.js"
+  },
+  {
+    "revision": "7b305e4856de269a6eb0",
+    "url": "/ip/static/js/runtime-main.21c93408.js"
+  }
+]);
